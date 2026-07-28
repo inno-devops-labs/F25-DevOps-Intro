c6f3eb22a3a40921af24d77f7462da6c1083d781
Mimir-sma <arsengobozov15region@gmail.com>
2026-07-28 10:52:35 +0300

    fix(lab9): remove vulnerable dependency demo
    Signed-off-by: Mimir-sma <arsengobozov15region@gmail.com>

--- app/go.mod
+++ app/go.mod
@@ -1,5 +1,3 @@
 module quicknotes
 
 go 1.24
-
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- app/vulnerable_demo.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import jwt "github.com/dgrijalva/jwt-go"
-
-// This file exists for one CI demonstration commit only.
-var vulnerableDemoResult = jwt.MapClaims{
-	"aud": []string{"unexpected"},
-}.VerifyAudience("quicknotes", false)
